b971984b3de0ded81a6fa4e82b2b08240de4da40
ju yeong <87563115+ju-yeong@users.noreply.github.com>
2021-12-02 16:59:39 +0900

    20211202

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,88 +1,137 @@
+// scroll top btn
+
+$('.scroll-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+})
+
 // us text loop
 
 function usText1() {
     setTimeout(function () {
-        $('.us-text1').css('display','none');
-        $('.us-text2').css('display','block');
+        $('.us-text1').css('display', 'none');
+        $('.us-text2').css('display', 'block');
         usText2()
-    }, 3500);
+    }, 5000);
 }
 function usText2() {
     setTimeout(function () {
-        $('.us-text2').css('display','none');
-        $('.us-text1').css('display','block');
+        $('.us-text2').css('display', 'none');
+        $('.us-text1').css('display', 'block');
         usText1()
-    }, 3500);
+    }, 5000);
 }
 
 usText1();
 
 // scroll event
 
-let lastScroll = 0,
-        delta = 10,
-        mainMove = true;
 
+let lastScrollTop = 0;
 
 $(window).scroll(function () {
+
     let scrollTop = $(this).scrollTop()
+    let mainMove = false
 
-    // console.log(scrollTop);
 
-    if(scrollTop > 90){
-        $( '.video-text' ).fadeOut();
+    // top btn
+
+
+    // console.log("scrolltop:" + scrollTop);
+
+    if (
+        (scrollTop < lastScrollTop)
+        &&
+        (scrollTop > 1500)
+    ) {
+        $(".scroll-top").css('transform', 'translateY(0)');
+    } else {
+        $(".scroll-top").css('transform', 'translateY(150px)');
+    };
+
+    lastScrollTop = scrollTop;
+
+
+    // video text
+    if (scrollTop > 90) {
+        $('.video-text').fadeOut();
 
     } else {
-        $( '.video-text' ).fadeIn();
+        $('.video-text').fadeIn();
     }
-    
-
-    
-    // setInterval(() => {
-    //     lastScroll = scrollTop;
-    // }, 1);
-
-    // if(
-    //     (scrollTop > 200) && (scrollTop < 230)
-    //     // Math.abs(lastScroll - scrollTop) <= delta
-    //     ) {
 
 
-    //         if(
-    //             scrollTop > lastScroll
-                
-    //             // Math.abs(lastScroll - scrollTop) <= delta
-                
-    //             ){
-
-    //         let offset = $('.us').offset();
-
-    //         $('html, body').animate({scrollTop : offset.top}, 400);
-
-
-            
-    //         console.log(`lastScroll: ${lastScroll}`)
-    //         console.log(`scrollTop: ${scrollTop}`)
-    //     }
-    // }
-
-
-    if(scrollTop < 70){
+    // move to main
+    if (scrollTop < 100) {
         mainMove = true;
     }
-    
-    if((scrollTop > 80) && (mainMove === true)){
+    // console.log(mainMove)
+    if (
+        ((scrollTop > 80) && (scrollTop < 200))
+        && (mainMove === true)
+    ) {
         let offset = $('.us').offset();
-        $('html, body').animate({scrollTop : offset.top}, 400);
+        $('html, body').animate({ scrollTop: offset.top }, 400);
+
+        // console.log(`mainMove: ${mainMove}`)
+        // console.log(`scrollTop: ${scrollTop}`)
 
         mainMove = false;
-        console.log('it working')
+        // console.log('it working')
     }
 
-    console.log(`mainMove: ${mainMove}`)
-    console.log(`scrollTop: ${scrollTop}`)
+    // experience bg img show hide
+
+
+    //상대좌표로 스크롤 제어하기
+    let target1 = document.querySelector('.ex-img1');
+    let target2 = document.querySelector('.ex-img2');
+    let target3 = document.querySelector('.ex-img3');
+
+    let elementTop1 = target1.getBoundingClientRect().top;
+    let elementTop2 = target2.getBoundingClientRect().top;
+    let elementTop3 = target3.getBoundingClientRect().top;
+
+    // console.log(elementTop1);
+
+    if ((elementTop1 > 100) && (elementTop1 < 500)) {
+        $('.ex-img1').addClass('opacity');
+    } else {
+        $('.ex-img1').removeClass('opacity');
+    }
+    if ((elementTop2 > 100) && (elementTop2 < 500)) {
+        $('.ex-img2').addClass('opacity');
+    } else {
+        $('.ex-img2').removeClass('opacity');
+    }
+    if ((elementTop3 > 100) && (elementTop3 < 500)) {
+        $('.ex-img3').addClass('opacity');
+    } else {
+        $('.ex-img3').removeClass('opacity');
+    }
 
 })
+
+// let lastScrollTop = 0;
+// $(window).scroll(function(){
+//     // top btn
+//     let scrollTop = $(this).scrollTop()
+
+
+//     console.log("scrolltop:" + scrollTop);
+
+//     if (
+//         (scrollTop > lastScrollTop)
+//         &&
+//         (scrollTop < 2000)
+//      ) {
+//         $(".scroll-top").css('transform', 'translateY(150px)');
+//     } else {
+//         $(".scroll-top").css('transform', 'translateY(0)');
+//     };
+
+//     lastScrollTop = scrollTop;
+// })
 
 
 // $('.video').click(function () {
